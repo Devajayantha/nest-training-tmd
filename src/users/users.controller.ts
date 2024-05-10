a0cb2f1fc891b9219e1a0168a93b1e575107a613
Devajayantha <devajayantha888@gmail.com>
2024-05-10 14:41:32 +0800

    imlement prisma

---
+++ src/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  public async create(@Body() data: any) {
+    return data;
+  }
+}
